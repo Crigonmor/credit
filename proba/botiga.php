247499d76d1da5cfdb7eeb8346e8aa2b4f332b95
cristian <cristian.gonzalez@manlleu.lasalle.cat>
2021-04-13 17:34:20 +0200

    s

---
+++ proba/botiga.php
@@ -0,0 +1,34 @@
+<?php include 'header.php' ?>
+
+
+
+<body>
+  
+  <div class="container">
+    <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+
+
+    <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+
+
+    <!-- Columns are always 50% wide, on mobile and desktop -->
+
+
+    <div class="row  pa d">
+  <div class="col-12 titol ">col</div>
+  <div class="col-3 co grafica">col1</div>
+  <div class="col-3 co">col</div>
+  <div class="col-3 co">col</div>
+  <div class="col-3 co">col</div>
+  <div class="col-3 text">347,99 € </div>
+  <div class="col-3 text">col</div>
+  <div class="col-3 text">col</div>
+  <div class="col-3 text">col</div>
+</div>
+
+
+</div>
+
+
+
+<?php include 'footer.php' ?>
---
+++ proba/footer.php
@@ -0,0 +1,80 @@
+<style>
+body {
+display: flex;
+min-height: 100vh;
+flex-direction: column;
+
+
+}
+.main {
+  flex: 1;
+
+}
+
+.footer {
+  background-color: #212529;
+  width: 100%;
+  min-height: 10px;
+}
+p{
+  margin-right: 10px;
+}
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
+.imatge{
+background-color: black;
+}
+header{
+  background-color: #4c4f51;
+}
+
+
+.proba{
+
+margin: 3px;
+  border: solid 1px;
+}
+
+.pa{
+  border: solid 1px red;
+
+}
+.co{
+  border: solid 1px yellow;
+  height: 300px;
+
+}
+.text{
+    border: solid 1px black;
+}
+.titol{
+  height: 50px;
+  border: solid 1px blue;
+}
+.grafica{
+  background-image: url('https://m.media-amazon.com/images/I/613ZzkRXGyL._AC_UL320_.jpg');
+  background-repeat:no-repeat;
+}
+</style>
+</head>
+<body>
+
+
+<div class='main'></div>
+<footer class="footer">
+<div class="container text-white" >
+  <br>
+  <ul class=" d-flex justify-content-center  my-1 my-lg-3 me-lg- ">
+      <p>Tot informatica</p><p>Tot informatica</p> <p>Tot informatica</p>
+
+
+    </div>
+
+    </div>
+  </div>
+</div>
+</footer>
+</body>
