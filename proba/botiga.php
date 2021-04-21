4c45d8e72620f0364b3cef8c03283f6222e27f1b
OriolQP <oriolquintanilla2@gmail.com>
2021-04-21 17:45:47 +0200

    p

--- proba/botiga.php
+++ proba/botiga.php
@@ -10,10 +10,10 @@
 
     <div class="row   d">
 
-  <div class="col-6  order-6  order-first order-sm-6 col-sm-6 col-sm-6 col-lg-3 col-xl-7 order-lg-8 order-xl-8 co grafica">col2</div>
-  <div class="col-6 order-7  col-sm-6  order-sm-7  col-lg-3 order-lg-8  col-xl-1  order-xl-1  im-2 co">col3</div>
-  <div  class="col-6 order-2 col-sm-6  order-sm-2 col-lg-3 order-lg-3  col-xl-4  order-xl-2  im-3   co ">col4</div>
-  <div class="col-6  order-3  col-sm-6 order-sm-3    col-lg-3 order-lg-9  col-xl-3 order-xl-6  im-4   co">col5</div>
+  <div class="col-6  order-6  order-first order-sm-6 col-sm-6 col-sm-6 col-lg-3 col-xl-7 order-lg-8 order-xl-8 co grafica"></div>
+  <div class="col-6 order-7  col-sm-6  order-sm-7  col-lg-3 order-lg-8  col-xl-1  order-xl-1  im-2 co"></div>
+  <div  class="col-6 order-2 col-sm-6  order-sm-2 col-lg-3 order-lg-3  col-xl-4  order-xl-2  im-3   co "></div>
+  <div class="col-6  order-3  col-sm-6 order-sm-3    col-lg-3 order-lg-9  col-xl-3 order-xl-6  im-4   co"></div>
   <div class="col-6 order-9  col-sm-6  order-sm-9  col-lg-3 order-lg-6  col-xl-2 order-xl-4      text">347,99 € </div>
   <div class="col-6 order-8   col-sm-6 order-sm-8   col-lg-3 order-lg-2 col-xl-5  order-xl-5     text">459,90 €</div>
   <div class="col-6 order-4 col-sm-6   order-sm-4 col-lg-3 order-lg-4  col-xl-6  order-xl-5   text">110 € </div>
@@ -21,10 +21,10 @@
 </div>
 <div class="row   d">
 
-<div class="col-6  order-6  order-first order-sm-6 col-sm-6 col-sm-6 col-lg-3 col-xl-7 order-lg-8 order-xl-8 co im-5">col2</div>
-<div class="col-6 order-7  col-sm-6  order-sm-7  col-lg-3 order-lg-8  col-xl-1  order-xl-1  im-6 co">col3</div>
-<div  class="col-6 order-2 col-sm-6  order-sm-2 col-lg-3 order-lg-3  col-xl-4  order-xl-2  im-7   co ">col4</div>
-<div class="col-6  order-3  col-sm-6 order-sm-3    col-lg-3 order-lg-9  col-xl-3 order-xl-6  im-8   co">col5</div>
+<div class="col-6  order-6  order-first order-sm-6 col-sm-6 col-sm-6 col-lg-3 col-xl-7 order-lg-8 order-xl-8 co im-5"></div>
+<div class="col-6 order-7  col-sm-6  order-sm-7  col-lg-3 order-lg-8  col-xl-1  order-xl-1  im-6 co"></div>
+<div  class="col-6 order-2 col-sm-6  order-sm-2 col-lg-3 order-lg-3  col-xl-4  order-xl-2  im-7   co "></div>
+<div class="col-6  order-3  col-sm-6 order-sm-3    col-lg-3 order-lg-9  col-xl-3 order-xl-6  im-8   co"></div>
 <div class="col-6 order-9  col-sm-6  order-sm-9  col-lg-3 order-lg-6  col-xl-2 order-xl-4      text">347,99 € </div>
 <div class="col-6 order-8   col-sm-6 order-sm-8   col-lg-3 order-lg-2 col-xl-5  order-xl-5     text">135 €</div>
 <div class="col-6 order-4 col-sm-6   order-sm-4 col-lg-3 order-lg-4  col-xl-6  order-xl-5   text">130 €</div>
