5fd0a61a9dc4bbd5377c5caadeb23a679e64f8be
OriolQP <oriolquintanilla2@gmail.com>
2021-04-21 17:35:31 +0200

    m

--- proba/header.php
+++ proba/header.php
@@ -5,7 +5,7 @@
     <head>
       <meta charset="utf-8">
 
-      <title>Headers · Bootstrap v5.0</title>
+      <title>Tot Informàtica</title>
       <link href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" rel="stylesheet">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
